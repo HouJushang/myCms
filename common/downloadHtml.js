0a4ef0839526a17ae012e9915f457fa82d065cd0
后居上 <test@123.com>
2017-05-26 17:24:22 +0800

    loadHtml

---
+++ common/downloadHtml.js
@@ -0,0 +1,23 @@
+/**
+ * Created by hou on 2017/5/26.
+ */
+const request = require('request')
+module.exports = function (url) {
+    return new Promise((resolve, reject) => {
+        request({
+            url : url,
+            method : 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+            }
+        },function(err, red, body) {
+            //请求到body
+            if(err){
+                console.error('[ERROR]Collection' + err);
+                reject(err)
+                return;
+            }
+            resolve(body)
+        })
+    })
+}
--- router/downloadImg.js
+++ router/downloadImg.js
@@ -3,12 +3,16 @@
  */
 const Router = require('./Router');
 const dImg = require('../common/downloadImg');
+const dHtml = require('../common/downloadHtml')
 const dateFormat = require('../util/dateFormat')
 Router.post('downloadImg', '/downloadimg', async function (ctx, next) {
     const bodyData = ctx.request.body;
-    const imageData = await dImg.image({
-        url: bodyData.url,
-        dest: `./public/uploads/`
+    // const imageData = await dImg.image({
+    //     url: bodyData.url,
+    //     dest: `./public/uploads/`
+    // })
+    // console.log(imageData)
+    dHtml(bodyData.url).then(e=>{
+        console.log(e)
     })
-    console.log(imageData)
 });
