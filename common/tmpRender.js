82ea7670c6754b6e1dc390e71994212d976b69c2
后居上 <test@123.com>
2017-06-12 17:20:24 +0800

    dev

--- common/tmpRender.js
+++ common/tmpRender.js
@@ -1,19 +1,22 @@
 const njk = require('nunjucks')
 const tmpPath = require('../config/website').tmpPath
+const pubPath = require('../config/website').pubPath
 const fs = require('fs')
+const path = require('path')
 /**
  * @param tmp 模板路径
  * @param tmpData 模板数据
  * @param htmlPath html路径
  * @returns {*|String} 返回html
  */
-function render(tmp, tmpData, htmlPath) {
+function render(tmp, tmpData, htmlPath, filename) {
     tmpData.tmpPath = tmpPath
     var env = new njk.Environment(new njk.FileSystemLoader(tmpPath));
     var result = env.render(tmp, tmpData)
-
+    var htmlFolder = path.join(pubPath, htmlPath)
+    console.log(htmlFolder)
     fs.createWriteStream(htmlPath)
-    fs.writeFile(htmlPath, result, function(err) {
+    fs.writeFile(htmlFolder, result, function(err) {
         if(err) {
             return 'fail';
         }
--- router/doPass.js
+++ router/doPass.js
@@ -3,9 +3,18 @@
  */
 const Router = require('./Router')
 const artModel = require('../model/article')
+const catModel = require('../model/category')
+const tmpRender = require('../common/tmpRender')
+
 Router.post('doPass', '/dopass',async function (ctx, next) {
     const bodyData = ctx.request.body;
     const artData = await artModel.findOne({id: bodyData.id});
+    const catData = await catModel.findOne({id: artData.categoryId})
+
+    //发布生成html
+    tmpRender(catData.atricle_tmp, artData, `catData.folder/${artData.id}.html`)
+    return
+    // 发布成功更改状态
     const result = await artModel.update({status: 1},{
         where: {
             id: bodyData.id
